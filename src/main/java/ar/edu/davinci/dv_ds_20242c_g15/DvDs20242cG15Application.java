28ef0ed4ec6d2a64d22d4280897e4478f5f4f903
Elian-Bufi <eliangastonbufi@gmail.com>
2024-11-30 19:06:54 -0300

    Union de maven con el proyecto en git

---
+++ src/main/java/ar/edu/davinci/dv_ds_20242c_g15/DvDs20242cG15Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20242c_g15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20242cG15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20242cG15Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dv_ds_20242c_g15/DvDs20242cG15ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20242c_g15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20242cG15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
